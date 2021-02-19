44d423f111b3e5271697d6bf0f40aeef2547713d
nintenbox99@outlook.jp <nintenbox99@outlook.jp>
2021-02-19 20:06:41 +0900

    added 2 operators

--- __init__.py
+++ __init__.py
@@ -35,12 +35,18 @@ def GetCamPos(spaceView3d):
 
 class QuantizeOp(bpy.types.Operator):
     bl_idname = "object.quantize_op"
-    bl_label = "My Coordinate Quantize Operator"
+    bl_label = "CoQuantize"
+    bl_description = "quantize coordinates of selected vertices.\n"\
+        "for example, if (1.23, 1.26, 1.27) is converted into (1.25, 1.25, 1.25).\n"\
+        "each elt is calculated like round(x * N) / N.\n"\
+        "now, N is fixed to 16. it is my fault, i'm not accustomed to Blender Add on."
     
     quantize_val: bpy.props.IntProperty(default=16)
     
     @classmethod
     def poll(cls, context):
+        if not context.mode == 'OBJECT':
+            return False
         return (context.active_object is not None) and (context.active_object.type == 'MESH')
     
     def execute(self, context):
@@ -50,21 +56,142 @@ class QuantizeOp(bpy.types.Operator):
                 y = round(v.co.y * self.quantize_val) / self.quantize_val
                 z = round(v.co.z * self.quantize_val) / self.quantize_val
                 v.co = Vector((x, y, z))
-                print(v.co)
         return {'FINISHED'}
+
+class QuantizeUvOp(bpy.types.Operator):
+    bl_idname = "object.quantize_uv_op"
+    bl_label = "UV Quantize"
+    bl_description = "quantize UV values of selected vertices in UV editor.\n"\
+        "u will be set to round(u * N) / N.\n"\
+        "N is width of an image, which is found in the node first, for now.\n"
+    
+    @classmethod
+    def poll(cls, context):
+        if not context.mode == 'OBJECT':
+            return False
+    
+        try:
+            # check if we can access desired data from this context.
+            _ = context.object.material_slots[0].material.node_tree.nodes
+        except:
+            return False
+        
+        return True
+    
+    def execute(self, context):
+        imgWidth = 0
+        for node in context.object.material_slots[0].material.node_tree.nodes:
+            if node.type == 'TEX_IMAGE':
+                imgWidth = node.image.size[0]
+        if imgWidth == 0:
+            return {'FINISHED'}
+        
+        for i in context.object.data.uv_layers[0].data:
+            if not i.select:
+                continue
+            u = round(i.uv[0] * imgWidth) / imgWidth
+            v = round(i.uv[1] * imgWidth) / imgWidth
+            i.uv = Vector((u, v))
+        return {'FINISHED'}
+
+class MoveDepthOp(bpy.types.Operator):
+    bl_idname = "object.move_depth_op"
+    bl_label = "MoveDepth"
+    bl_description = "you can move an object without changing its position in the screen.\n"\
+        "this operator may be useful for setting up a pose with IK."
+    
+    def __init__(self):
+        self.prevMousePos = Vector((0.0, 0.0))
+        self.spd = 0.3
+        self.posBeforeOp = Vector((0.0, 0.0, 0.0))
+    
+    @classmethod
+    def poll(cls, context):
+        if context.mode != 'OBJECT':
+            return False
+        elif context.active_object is None:
+            #print("no object is selected???")
+            return False
+        elif not context.active_object.type == "MESH":
+            #print("selected object is not a mesh????")
+            return False
+        elif not type(context.space_data) is bpy.types.SpaceView3D:
+            #print("ctx.space_data is not bpy.types.SpaceView3D")
+            return False
+        return True
+    
+    def execute(self, context):
+        return {'FINISHED'}
+    
+    def invoke(self, context, event):
+        context.window_manager.modal_handler_add(self)
+        self.prevMousePos = Vector((event.mouse_x, event.mouse_y))
+        
+        # context.object.location is not a value, but a reference! so create a new vector.
+        self.posBeforeOp = Vector(context.object.location)
+        
+        print("oh posBeforeOp was set just now")
+        return {'RUNNING_MODAL'}
+    
+    def modal(self, context, event):
+        if event.type == 'MOUSEMOVE':
+            mousePos = Vector((event.mouse_x, event.mouse_y))
+            dPos = mousePos - Vector((event.mouse_prev_x, event.mouse_prev_y))
+            objPos = context.object.location
+            curCamPos = GetCamPosFromCtx(context)
+            dv = objPos - curCamPos
+            if dv.length > 0:
+                newLength = dv.length + dPos.y * self.spd
+                newDv = dv / dv.length * newLength
+                context.object.location = curCamPos + newDv
+        elif event.type in {'RIGHTMOUSE', 'ESC'}:
+            context.object.location = self.posBeforeOp
+            return {'CANCELLED'}
+        elif event.type in {'LEFTMOUSE'}:
+            return {'FINISHED'}
+        
+        return {'RUNNING_MODAL'}
+
+class MyOperatorsPanel(bpy.types.Panel):
+    bl_idname = "VIEW3DT_PT_myoperators"
+    bl_label = "MyOperators"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'Tool'
+    
+    @classmethod
+    def poll(cls, context):
+        return (context.object is not None)
+    def draw_header(self, context):
+        layout = self.layout
+        layout.label(icon='OBJECT_DATA')
+    
+    def draw(self, context):
+        layout = self.layout
+        
+        layout.operator(MoveDepthOp.bl_idname)
+        layout.operator(QuantizeOp.bl_idname)
+        layout.operator(QuantizeUvOp.bl_idname)
+    
+
+
+classes = [QuantizeOp, QuantizeUvOp, MoveDepthOp, MyOperatorsPanel]
 
 def menu_func(self, context):
     self.layout.operator(QuantizeOp.bl_idname)
+    self.layout.operator(MoveDepthOp.bl_idname)
 
 def register():
-    bpy.utils.register_class(QuantizeOp)
+    for i in classes:
+        bpy.utils.register_class(i)
     bpy.types.VIEW3D_MT_object.append(menu_func)
 
 def unregister():
-    bpy.utils.unregister_class(QuantizeOp)
+    for i in classes:
+        bpy.utils.unregister_class(i)
 
 
 if __name__ == '__main__':
     register()
-    bpy.ops.object.quantize_op("INVOKE_DEFAULT")
+    
 
